f5e8ac1b98cd847ab629e5543b8963acd37b45d1
JoshClopton <joshclopton@gmail.com>
2022-05-16 15:47:41 -0700

    add Shelves component

---
+++ src/components/Shelves/Shelves.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { CardComponent } from "../CardComponent/CardComponent";
+import "./Shelves.scss";
+
+export const Shelves = () => {
+	return (
+		<div>
+			<Header />
+			<CardComponent />
+		</div>
+	);
+};

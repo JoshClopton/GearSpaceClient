433753850afaec4d2a2a853513f339699ef855b6
JoshClopton <joshclopton@gmail.com>
2022-05-15 20:53:08 -0700

    change axios call to link to back-end

--- src/components/LoginButtonComponent/LoginButtonComponent.js
+++ src/components/LoginButtonComponent/LoginButtonComponent.js
@@ -1,9 +1,30 @@
+import axios from "axios";
 import React from "react";
 import "./LoginButtonComponent.scss";
 
 //this is the component for the site logo
 const LoginButtonComponent = () => {
-	return <button className="login-button">Log In</button>;
+	return (
+		// <button
+		// 	onClick={() => {
+		// 		axios
+		// 			.get(`http://localhost:8000/auth/auth/google`, {
+		// 				// withCredentials: true,
+		// 			})
+		// 			.then((res) => {
+		// 				console.log("🕵🏻‍♂️ res: ", res); //TODO: remove/comment
+
+		// 				// Update the state: done authenticating, user is logged in, set the profile data
+		// 			});
+		// 	}}
+		// 	className="login-button"
+		// >
+		// 	Log In
+		// </button>
+		<a className="login-button" href={"http://localhost:8000/auth/google"}>
+			link
+		</a>
+	);
 };
 
 export default LoginButtonComponent;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,13 +4,15 @@ import logoImage from "../assets/logo/BunchofFives_WEB_BW_800x800px.jpg";
 import LogoComponent from "../components/LogoComponent/LogoComponent";
 import LoginFormComponent from "../components/LoginFormComponent/LoginFormComponent";
 import "./HomePage.scss";
+import LoginButtonComponent from "../components/LoginButtonComponent/LoginButtonComponent";
 
 const HomePage = () => {
 	return (
 		<div className="home-page">
-			<LogoComponent />
+			{/* <LogoComponent />
 			<h1>GearSpace</h1>
-			<LoginFormComponent />
+			<LoginFormComponent /> */}
+			<LoginButtonComponent />
 		</div>
 	);
 };

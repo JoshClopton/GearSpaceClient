5d8c2a531b116aa052ed82a99b694572fc1dc501
JoshClopton <joshclopton@gmail.com>
2022-05-15 22:23:00 -0700

    add header feature, add link to logo

---
+++ src/components/Header/Header.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import LogoComponent from "../LogoComponent/LogoComponent";
+// import LoginButtonComponent from "../components/LoginButtonComponent/LoginButtonComponent";
+import "./Header.scss";
+
+export const Header = () => {
+	return (
+		<div className="header">
+			<div className="header__title-logo-container">
+				<LogoComponent />
+				<h1 className="header__title">Gear Space</h1>
+			</div>
+			<form className="search-form">
+				<input
+					className="search-form__input"
+					type="text"
+					id="search"
+					name="search"
+					placeholder="Search by category"
+				/>
+			</form>
+
+			<a
+				className="header__login-link"
+				activeClassName="header__login-link--active"
+				href={"http://localhost:8000/auth/google"}
+			>
+				Sign In
+			</a>
+		</div>
+	);
+};
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,14 +5,16 @@ import LogoComponent from "../components/LogoComponent/LogoComponent";
 import LoginFormComponent from "../components/LoginFormComponent/LoginFormComponent";
 import "./HomePage.scss";
 import LoginButtonComponent from "../components/LoginButtonComponent/LoginButtonComponent";
+import { Header } from "../components/Header/Header";
 
 const HomePage = () => {
 	return (
 		<div className="home-page">
+			<Header />
 			{/* <LogoComponent />
 			<h1>GearSpace</h1>
 			<LoginFormComponent /> */}
-			<LoginButtonComponent />
+			{/* <LoginButtonComponent /> */}
 		</div>
 	);
 };

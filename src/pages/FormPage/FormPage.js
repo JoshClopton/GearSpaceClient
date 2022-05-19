4836bc8f7ff70a11d87ed56e418606d9c923be82
JoshClopton <joshclopton@gmail.com>
2022-05-19 13:34:53 -0700

    add shelve items to axios call from form-page

--- src/pages/FormPage/FormPage.js
+++ src/pages/FormPage/FormPage.js
@@ -23,6 +23,9 @@ export const FormPage = () => {
 								shelf: e.shelf,
 								description: e.description,
 								item: e.item,
+								location: e.location,
+								qty: e.qty,
+								notes: e.notes,
 							},
 							{ withCredentials: true }
 						)
@@ -43,6 +46,13 @@ export const FormPage = () => {
 					<Field id="item" name="item" />
 					<label htmlFor="description">Description</label>
 					<Field id="description" name="description" />
+					<label htmlFor="qty">Quantity</label>
+					<Field id="qty" name="qty" />
+					<label htmlFor="location">Location</label>
+					<Field id="location" name="location" />
+					<label htmlFor="notes">Notes</label>
+					<Field id="notes" name="notes" />
+
 					<button type="submit">Submit</button>
 				</Form>
 			</Formik>

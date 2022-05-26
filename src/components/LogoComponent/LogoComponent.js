6d0ea6988c90faf5f6d34aa6efca8b94c60555d1
JoshClopton <joshclopton@gmail.com>
2022-05-25 20:09:33 -0700

    final commit

--- src/components/LogoComponent/LogoComponent.js
+++ src/components/LogoComponent/LogoComponent.js
@@ -3,7 +3,6 @@ import logoImage from "../../assets/logo/BunchofFives_WEB_BW_800x800px.png";
 import "./LogoComponent.scss";
 import { NavLink } from "react-router-dom";
 
-//this is the component for the site logo
 const LogoComponent = () => {
 	return (
 		<NavLink to="/" className="logo">

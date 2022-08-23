d1969507bdc6c896d1afee60d887cf3a05965e99
JoshClopton <joshclopton@gmail.com>
2022-08-23 13:34:33 +0200

    clean up header code

--- src/components/EditShelf/EditShelf.js
+++ src/components/EditShelf/EditShelf.js
@@ -1,6 +1,7 @@
 import React from "react";
+import EditForm from "../EditForm/EditForm";
 import "./EditShelf.scss";
-import { Formik, Field, Form } from "formik";
+import { Formik } from "formik";
 import axios from "axios";
 import { useState } from "react";
 
@@ -13,86 +14,45 @@ const EditShelf = (props) => {
 		? "modal--display overlay--display"
 		: "modal--hide overlay--hide";
 
-	const toggleOverlay = show ? "overlay--display" : "overlay--hide";
-
 	return (
-		<>
-			<div className={toggleModal}>
-				<div className="modal">
-					<Formik
-						initialValues={{
-							picked: "",
-							shelf: "",
-							item: "",
-							qty: "",
-							location: "",
-							notes: "",
-							image: "",
-						}}
-						onSubmit={(e, values) => {
-							axios
-								.put(
-									{
-										shelf: e.shelf,
-										location: e.location,
-										qty: e.qty,
-										notes: e.notes,
-									},
-									{ withCredentials: true }
-								)
-								.catch((err) => {
-									console.log("Error creating a new post:", err);
-								});
-						}}
-					>
-						<Form>
-							<label>
-								<Field type="radio" name="shelf" value="shelter" />
-								Shelter
-							</label>
-							<label>
-								<Field type="radio" name="shelf" value="cook" />
-								Cook
-							</label>
-							<label>
-								<Field type="radio" name="shelf" value="lighting" />
-								Lighting
-							</label>
-							<label>
-								<Field type="radio" name="shelf" value="clothing" />
-								Clothing
-							</label>
-							<label>
-								<Field type="radio" name="shelf" value="optics" />
-								Optics
-							</label>
-							<label>
-								<Field type="radio" name="shelf" value="packs" />
-								Packs
-							</label>
-
-							<label>
-								<Field type="radio" name="shelf" value="other-gear" />
-								Other Gear
-							</label>
-							<label htmlFor="qty">Quantity</label>
-							<Field id="qty" name="qty" />
-							<label htmlFor="location">Location</label>
-							<Field id="location" name="location" />
-							<label htmlFor="notes">Notes</label>
-							<Field id="notes" name="notes" />
-							<button type="submit">Submit</button>
-						</Form>
-					</Formik>
-					<span className="modal__text"></span>
-					<div className="button-container">
-						<button onClick={handleShow} className="delete-button">
-							Cancel
-						</button>
-					</div>
+		<div className={toggleModal}>
+			<div className="modal">
+				<Formik
+					initialValues={{
+						picked: "",
+						shelf: "",
+						item: "",
+						qty: "",
+						location: "",
+						notes: "",
+						image: "",
+					}}
+					onSubmit={(e) => {
+						axios
+							.put(
+								{
+									shelf: e.shelf,
+									location: e.location,
+									qty: e.qty,
+									notes: e.notes,
+								},
+								{ withCredentials: true }
+							)
+							.catch((err) => {
+								console.log("Error creating a new post:", err);
+							});
+					}}
+				>
+					<EditForm />
+				</Formik>
+				<span className="modal__text"></span>
+				<div className="button-container">
+					<button onClick={handleShow} className="delete-button">
+						Cancel
+					</button>
 				</div>
 			</div>
-		</>
+		</div>
 	);
 };
 

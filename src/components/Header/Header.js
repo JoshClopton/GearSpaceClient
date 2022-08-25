9e690282cb32dcc4bd2f21090553edf28299bc01
JoshClopton <joshclopton@gmail.com>
2022-08-25 13:54:57 +0200

    fix merge conflicts

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,18 @@
 import axios from "axios";
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import Logo from "../Logo/Logo";
 import "./Header.scss";
 import LoginForm from "../LoginForm/LoginForm";
 
-export const Header = ({
-  loggedIntoGoogle,
-  loggedOutOfGoogle,
-  handleLoggedIn,
-}) => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
+const Header = ({ loggedIntoGoogle, loggedOutOfGoogle, isLoggedIn }) => {
+  // //On page load, a get request is sent to the back end to check if the session has been authenticated. If so, set logged in to true in home page. Otherwise, set logged in to false.
   useEffect(() => {
+    console.log("🕵🏻‍♂️ Header UseEffect ran: "); //TODO: remove/comment
+
     axios
       .get("http://localhost:8000/auth/profile", { withCredentials: true })
       .then((res) => {
-        res.data.google_id ? setIsLoggedIn(true) : setIsLoggedIn(false);
-        res.data.google_id && handleLoggedIn();
+        res.data.google_id ? loggedIntoGoogle() : loggedOutOfGoogle();
       });
   }, [loggedIntoGoogle, loggedOutOfGoogle]);
 
